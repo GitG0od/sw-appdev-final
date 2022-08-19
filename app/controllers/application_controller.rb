8fe2225076b559f5cb7f72a3c242266d1f20d7d7
GitG0od <swachte0@chicagobooth.edu>
2022-08-19 02:36:10 +0000

    Works, but can't get @big_sword to false

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,18 @@ class ApplicationController < ActionController::Base
   
   def index
 
-    #@big_sword = false
+    #A function to let me convert strings to booleans
+    def to_boolean(str)
+      str == 'true'
+    end
+
+
+
+    if params = {}  #This is not durable because I could later add other params; ideally it would just check if params.fetch("big_sword") comes up blank, but I'm not sure how to do that and this works for now.
+      @big_sword = true
+    else
+      @big_sword = params.fetch(:big_sword).to_boolean
+    end
 
 if @big_sword == true or @big_sword == nil
     @shield = 0
@@ -24,5 +35,15 @@ end
 
 
   render({ :template => "pages/character1.html.erb" })
+  end
+
+
+  def change_equipment
+    if @big_sword == true or nil
+      params[:big_sword] = false
+    elsif @big_sword == false
+      params[:big_sword] = true
+    end
+    redirect_to("/")    # I could put in the following:   , {:notice => "Changed Equipment"} 
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
 
   get("/", { :controller => "application", :action => "index"} )
 
-  
+  get("/change_equipment", { :controller => "application", :action => "change_equipment"})
 
 end
