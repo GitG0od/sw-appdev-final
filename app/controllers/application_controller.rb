adb9dd2f4a2f8281756b42211d71774c63cc2aa8
GitG0od <swachte0@chicagobooth.edu>
2022-08-19 01:56:27 +0000

    Made binary sword and board or dual-wield

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,22 @@ class ApplicationController < ActionController::Base
   
   
   def index
+
+    #@big_sword = false
+
+if @big_sword == true or @big_sword == nil
+    @shield = 0
+    @weapon = "2d6"
+elsif @big_sword == false
+  @shield = 2
+    @weapon = "1d8"
+end
+
+
+
+    @armor = 14 + @shield
+
+
   render({ :template => "pages/character1.html.erb" })
   end
 end
